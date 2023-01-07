78aa0c5e63fe3d6d30b88a546642ba568804a676
Saphereye <adarshdas950@gmail.com>
2023-01-07 16:44:14 +0530

    Fixed #8, the regex now take all characters

--- markdown_parser.py
+++ markdown_parser.py
@@ -7,13 +7,13 @@ def markdown_to_gtk(text: str) -> str:
     # Bullet
     txt = re.sub(r"- (.+)\n", r"• \1\n", txt)
     # Bold
-    txt = re.sub(r"\*([0-9a-zA-Z ]+)\*", r"<b>\1</b>", txt)
+    txt = re.sub(r"\*(.+)\*", r"<b>\1</b>", txt)
     # Italics
-    txt = re.sub(r"/([0-9a-zA-Z ]+)/", r"<i>\1</i>", txt)
+    txt = re.sub(r"/(.+)/", r"<i>\1</i>", txt)
     # Big
     txt = re.sub(r"# (.+)\n", r"<big>\1</big>\n", txt)
     # Underline
-    txt = re.sub(r"\_([0-9a-zA-Z ]+)\_", r"<u>\1</u>", txt)
+    txt = re.sub(r"\_(.+)\_", r"<u>\1</u>", txt)
     # Subscript
     txt = re.sub(r"\_([0-9a-zA-Z]+)( |\n){1}", r"<sub>\1</sub>\2", txt)
     # Superscript
