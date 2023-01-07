e75ec18a8f31ca35fa877d2f673f0e396c80cddf
Saphereye <adarshdas950@gmail.com>
2023-01-07 18:16:57 +0530

    Fixed #5, used ascii box art

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ Mark-Editor
 
 Markdown editor made in python
 ![image](https://user-images.githubusercontent.com/59739923/211052460-fdbac291-432c-4515-80e0-7ab25b5a87c3.png)
+
+## Requirements
+### Modules
+- gi
+- re
--- markdown_parser.py
+++ markdown_parser.py
@@ -1,4 +1,5 @@
 import re
+from enum import Enum
 
 def markdown_to_gtk(text: str) -> str:
     txt = text
@@ -20,10 +21,60 @@ def markdown_to_gtk(text: str) -> str:
     txt = re.sub(r"\^([0-9a-zA-Z]+)( |\n){1}", r"<sup>\1</sup>\2", txt)
     # Monospace
     txt = re.sub(r"\`(.+)\`", r"<tt>\1</tt>", txt)
-    # Code Block
-    txt = re.sub(r"\`\`([\s\S]*?)\`\`", r"<tt>\1</tt>", txt)
+    # Table
+    tables = re.findall(r"\`\`\n([\s\S]*?)\n\`\`", txt)
+    print(tables)
+    while(tables != []):
+        tables = re.findall(r"\`\`\n([\s\S]*?)\n\`\`", txt)
+        substitute_str = markdown_to_pretty_table(tables.pop())
+        txt = re.sub(r"\`\`\n([\s\S]*?)\n\`\`", substitute_str, txt)
     # Strikethrough
     txt = re.sub(r"\~([0-9a-zA-Z ]+)\~", r"<s>\1</s>", txt)
     print(txt)
 
     return txt
+
+def markdown_to_pretty_table(table: str) -> str:
+    table = table.split('\n')
+    ptable: list[list[str]] = []
+
+    # Generate a 2d array containing all the items
+    ptable.append([i.strip() for i in table[0].split('|') if i != ''])
+    for i in range(2, len(table)):
+        ptable.append([i.strip() for i in table[i].split('|') if i != ''])
+    print(ptable)
+
+    # Calculate string with max length
+    max_len = 0
+
+    for i in ptable:
+        for j in i:
+            if len(j) > max_len:
+                max_len = len(j)
+    print(max_len)
+
+    # Print ptable formatted using max length
+    output_string = "┌" + ("─"*max_len + "┬")*(len(ptable[0])-1) + ("─"*max_len + "┐") + '\n'
+    row_num = 0
+    for i in ptable:
+        for j in i:
+            output_string += f"│{j.ljust(max_len)}"
+        row_num+=1
+        if row_num == 1:
+            output_string += "│\n├" + ("─"*max_len + "┼")*(len(ptable[0])-1) + f"{'─'*max_len}┤"
+        else:
+            output_string += "│"
+        output_string += '\n'
+    output_string += "└" + ("─"*max_len + "┴")*(len(ptable[0])-1) + ("─"*max_len + "┘") + '\n'
+
+
+    return output_string
+    
+
+if __name__ == "__main__":
+    table = """| Col 1 | Col 2 | Col 3  |
+|-------|-------|--------|
+| a     | 1     | 23     |
+| bh    | 4bh   | 45n hj |
+| a+b   | c*d   | 456    |"""
+    print(markdown_to_pretty_table(table))
